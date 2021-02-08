e26f7dab7603b4e04f9dc08e6ca3ec808823f6f5
neptune <angelacr@ymail.com>
2021-02-08 17:29:59 -0600

    adding three new files index.html script.js style. css

---
+++ script.js
@@ -0,0 +1,50 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+  passwordText.value = password;
+}
+
+var ucase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var special = "!@#$%^&*()_+-=<>?/;:";
+var lcase ="abcdefghijklmnopqrstuvwxyz";
+var useNumber = "0123456789";
+
+
+// Add event listener to generate button
+// Prompt and confirm what the user is asking for in password once button is clicked
+generateBtn.addEventListener("click", promptMe, writePassword);
+
+//make character selection mandatory user will determine how long the password will be
+function promptMe() {
+var characters = (prompt("How many characters?"));
+while(characters < 8 || characters > 128) {
+  alert("Min characters is 8 and max is 128");
+  var characters = (prompt("How many characters?"));
+}
+}
+
+//this part here is where i'm stuck and need to link the vars to do the thing
+ if(confirm("Do you want lowercase letters?")) {
+
+ }
+ 
+ if(confirm("Do you want uppercase letters?")) {
+
+ }
+
+ if(confirm("Do you want numbers?")) {
+ 
+ }
+
+if(confirm("Do you want special characters (ex. !@#$%)?")) {
+
+}
+
+
+
+
+//set random selector for vars, required to answer
